83e10d6817e40510c9e9fc2405f389be908348d0
Andrew Gunnerson <andrew.gunnerson@us.ibm.com>
2018-09-25 16:58:58 -0400

    40_env_kubernetes.sh: Remove shorthand for specifying the namespace
    It was really only necessary in older versions of kubectl where
    --namespace was supported, but -n was not.
    
    Signed-off-by: Andrew Gunnerson <andrew.gunnerson@us.ibm.com>

--- files/shell/shell.d/40_env_kubernetes.sh
+++ files/shell/shell.d/40_env_kubernetes.sh
@@ -1,66 +1,31 @@
-if which kubectl &>/dev/null; then
+if command -v kubectl &>/dev/null; then
     alias k='kubectl'
 
-    function kns_usage() {
+    kns_usage() {
         cat << EOF
-Usage: kns <pattern> [<kubectl arg> ...]
+Usage: kns <cluster> [<kubectl arg> ...]
 
-The <pattern> argument must be provided and takes the following forms:
+The <cluster> argument must be provided and is shorthand for
 
-* <config>
-  Use kubeconfig ~/.kube/config.<config> and default namespace
-* <config>/<namespace>
-  Use kubeconfig ~/.kube/config.<config> and namespace <namespace>
-* <config>/
-  Use kubeconfig ~/.kube/config.<config> and default namespace
-* /<namespace>
-  Use default kubeconfig and namespace <namespace>
-* /
-  Use default kubeconfig and default namespace
+    --kubeconfig ~/.kube/config.<cluster>
 
-Examples:
-* kns pd/jenkins ...
-  Use kubeconfig ~/.kube/config.pd and namespace 'jenkins'
-* kns pd ...
-  Use kubeconfig ~/.kube/config.pd and default namespace
-* kns /production
-  Use default kubeconfig and namespace 'production'
 EOF
     }
 
-    function kns() {
-        local config=
-        local namespace=
-        local kubeconfig=
-
-        if [[ "${1}" == -* ]]; then
+    kns() {
+        if [[ "${1}" == -* || "${1}" == */* ]]; then
             kns_usage >&2
             return 1
         fi
 
-        if [[ "${1}" == */* ]]; then
-            config=${1%%/*}
-            namespace=${1#*/}
-        else
-            config=${1}
-        fi
-
-        if [[ -z "${config}" ]]; then
-            kubeconfig=${HOME}/.kube/config
-        else
-            kubeconfig=${HOME}/.kube/config.${config}
-        fi
+        local kubeconfig=${HOME}/.kube/config.${1}
 
         if [[ ! -f "${kubeconfig}" ]]; then
             echo "${kubeconfig} does not exist" >&1
             return 1
         fi
 
-        if [[ -z "${namespace}" ]]; then
-            namespace=default
-        fi
-
         shift
-        kubectl --kubeconfig="${kubeconfig}" --namespace="${namespace}" "${@}"
+        kubectl --kubeconfig="${kubeconfig}" "${@}"
     }
 fi
