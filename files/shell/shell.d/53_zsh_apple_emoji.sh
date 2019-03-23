b98ce47882ee56c3d5f49c7b24a68c481e14b309
Andrew Gunnerson <andrewgunnerson@gmail.com>
2019-03-23 14:28:50 -0400

    53_zsh_apple_emoji.sh: Use replacement glyphs on every OS
    Signed-off-by: Andrew Gunnerson <andrewgunnerson@gmail.com>

--- files/shell/shell.d/53_zsh_apple_emoji.sh
+++ files/shell/shell.d/53_zsh_apple_emoji.sh
@@ -1,6 +1,5 @@
 # Avoid using glyphs overridden by the Apple Colored Emoji font
 # https://gitlab.com/gnachman/iterm2/issues/3384
-if is_os macOS && [[ -n "${ITERM_PROFILE}" ]]; then
-    ZSH_THEME_GIT_PROMPT_MODIFIED=${ZSH_THEME_GIT_PROMPT_MODIFIED:s/⚡/✹}
-    ZSH_THEME_GIT_PROMPT_UNMERGED=${ZSH_THEME_GIT_PROMPT_UNMERGED:s/♒/═}
-fi
+# The replacement glyphs are used by other oh-my-zsh themes anyway
+ZSH_THEME_GIT_PROMPT_MODIFIED=${ZSH_THEME_GIT_PROMPT_MODIFIED:s/⚡/✹}
+ZSH_THEME_GIT_PROMPT_UNMERGED=${ZSH_THEME_GIT_PROMPT_UNMERGED:s/♒/═}
