56f7c7e279ad6b0ea4ce160ad7269a4026c3c99c
Andrew Gunnerson <chillermillerlong@hotmail.com>
2020-06-18 16:28:54 -0400

    55_zsh_history.sh: Incrementally update history
    Signed-off-by: Andrew Gunnerson <chillermillerlong@hotmail.com>

--- files/shell/shell.d/55_zsh_history.sh
+++ files/shell/shell.d/55_zsh_history.sh
@@ -5,6 +5,10 @@ if is_shell zsh; then
     # Save the time and duration
     setopt EXTENDED_HISTORY
 
+    # Update history file incrementally. oh-my-zsh no longer does this by
+    # default as of: https://github.com/ohmyzsh/ohmyzsh/pull/8048
+    setopt inc_append_history
+
     # Save more history
     HISTSIZE=50000
     SAVEHIST=50000
