a8a83c62739163ad670849cc6b9c4456dfd3c12c
Andrew Gunnerson <andrew.gunnerson@us.ibm.com>
2020-08-27 18:14:05 -0400

    10_alias_man.sh: Don't add man() function if MANPAGER is set
    Signed-off-by: Andrew Gunnerson <andrew.gunnerson@us.ibm.com>

--- files/shell/shell.d/10_alias_man.sh
+++ files/shell/shell.d/10_alias_man.sh
@@ -1,19 +1,21 @@
-if command -v bat >/dev/null && [[ -z "${MANPAGER}" ]]; then
-    # base16 theme doesn't colorize much in man pages
-    export MANPAGER="sh -c 'col -bx | bat -l man -p'"
-else
-    man() {
-        # From website:
-        # http://www.cyberciti.biz/faq/linux-unix-colored-man-pages-with-less-command/
-        # https://wiki.archlinux.org/index.php/Man_Page
+if [[ -z "${MANPAGER}" ]]; then
+    if command -v bat >/dev/null; then
+        # base16 theme doesn't colorize much in man pages
+        export MANPAGER="sh -c 'col -bx | bat -l man -p'"
+    else
+        man() {
+            # From website:
+            # http://www.cyberciti.biz/faq/linux-unix-colored-man-pages-with-less-command/
+            # https://wiki.archlinux.org/index.php/Man_Page
 
-        LESS_TERMCAP_mb="$(printf "\e[1;31m")" \
-        LESS_TERMCAP_md="$(printf "\e[1;31m")" \
-        LESS_TERMCAP_me="$(printf "\e[0m")" \
-        LESS_TERMCAP_se="$(printf "\e[0m")" \
-        LESS_TERMCAP_so="$(printf "\e[1;44;33m")" \
-        LESS_TERMCAP_ue="$(printf "\e[0m")" \
-        LESS_TERMCAP_us="$(printf "\e[1;32m")" \
-        man "${@}"
-    }
+            LESS_TERMCAP_mb="$(printf "\e[1;31m")" \
+            LESS_TERMCAP_md="$(printf "\e[1;31m")" \
+            LESS_TERMCAP_me="$(printf "\e[0m")" \
+            LESS_TERMCAP_se="$(printf "\e[0m")" \
+            LESS_TERMCAP_so="$(printf "\e[1;44;33m")" \
+            LESS_TERMCAP_ue="$(printf "\e[0m")" \
+            LESS_TERMCAP_us="$(printf "\e[1;32m")" \
+            man "${@}"
+        }
+    fi
 fi
