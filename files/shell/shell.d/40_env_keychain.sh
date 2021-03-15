3efaee9e4d7513999c2111d93571525d840d9982
Andrew Gunnerson <chillermillerlong@hotmail.com>
2021-03-15 16:45:03 -0400

    shell: Use socat + npiperelay instead of keychain/ssh-agent in WSL
    Signed-off-by: Andrew Gunnerson <chillermillerlong@hotmail.com>

--- files/shell/shell.d/40_env_keychain.sh
+++ files/shell/shell.d/40_env_keychain.sh
@@ -1,5 +1,6 @@
 if command -v keychain &>/dev/null \
-        && (is_os WSL || (! is_os macOS \
-                && [[ "${XDG_CURRENT_DESKTOP}" != GNOME ]])); then
-    eval $(keychain -q --eval)
+        && ! is_os WSL \
+        && ! is_os macOS \
+        && [[ -z "${SSH_AUTH_SOCK}" ]]; then
+    eval "$(keychain -q --eval)"
 fi
---
+++ files/shell/shell.d/40_env_wsl_npiperelay.sh
@@ -0,0 +1,14 @@
+if is_os WSL \
+        && command -v npiperelay.exe &>/dev/null \
+        && command -v socat &>/dev/null; then
+    export SSH_AUTH_SOCK=${HOME}/.ssh/agent.sock
+
+    if ! ss -a | grep -qF "${SSH_AUTH_SOCK}"; then
+        rm -f "${SSH_AUTH_SOCK}"
+        (
+            setsid socat \
+                UNIX-LISTEN:"${SSH_AUTH_SOCK}",fork \
+                EXEC:"npiperelay.exe -ei -s //./pipe/openssh-ssh-agent",nofork &
+        ) #&>/dev/null
+    fi
+fi
