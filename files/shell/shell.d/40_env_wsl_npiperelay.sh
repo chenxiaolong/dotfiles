133656ffac9fd5b3d9cdc86e35218386ee87cc33
Andrew Gunnerson <chillermillerlong@hotmail.com>
2021-03-20 17:00:16 -0400

    40_env_wsl_npiperelay.sh: Use file locking to avoid race conditions when multiple shells start at the same time
    Signed-off-by: Andrew Gunnerson <chillermillerlong@hotmail.com>

--- files/shell/shell.d/40_env_wsl_npiperelay.sh
+++ files/shell/shell.d/40_env_wsl_npiperelay.sh
@@ -5,9 +5,13 @@ if is_os WSL \
         local win_socket=${1}
         local lin_socket=${2}
 
-        if ! socat OPEN:/dev/null UNIX-CONNECT:"${lin_socket}" &>/dev/null; then
-            rm -f "${lin_socket}"
-            (
+        (
+            exec {lock_fd}> "${lin_socket}.lock"
+            flock "${lock_fd}"
+
+            if ! socat OPEN:/dev/null UNIX-CONNECT:"${lin_socket}" &>/dev/null; then
+                rm -f "${lin_socket}"
+
                 # Switch to helper script directory and also use a helper script
                 # for npiperelay so that socat never has a chance to see a space
                 # in the EXEC: argument.
@@ -16,12 +20,15 @@ if is_os WSL \
                 export WIN_SOCKET=${win_socket}
 
                 if cd "${script_dir}"; then
+                    # socat should not inherit the lock file's file descriptor
                     setsid socat \
                         UNIX-LISTEN:"${lin_socket}",fork \
-                        EXEC:'./npiperelay_helper.sh',nofork &
+                        EXEC:'./npiperelay_helper.sh',nofork \
+                        {lock_fd}>&- \
+                        &
                 fi
-            )
-        fi
+            fi
+        )
     }
 
     export SSH_AUTH_SOCK=${HOME}/.ssh/agent.sock
