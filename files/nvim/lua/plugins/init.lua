b4659b872c5820752af42b1e7afa4fcba902f6c2
Andrew Gunnerson <chillermillerlong@hotmail.com>
2021-12-31 21:23:48 -0500

    nvim: nvim-cmp: Add vsnip and update keybindings
    Signed-off-by: Andrew Gunnerson <chillermillerlong@hotmail.com>

--- files/nvim/lua/plugins/init.lua
+++ files/nvim/lua/plugins/init.lua
@@ -56,7 +56,12 @@ return require('packer').startup(
         -- LSP autocompletion
         use {
             'hrsh7th/nvim-cmp',
-            requires = {'hrsh7th/cmp-nvim-lsp', 'hrsh7th/cmp-buffer'},
+            requires = {
+                'hrsh7th/cmp-buffer',
+                'hrsh7th/cmp-nvim-lsp',
+                'hrsh7th/cmp-vsnip',
+                'hrsh7th/vim-vsnip',
+            },
             config = function() require('plugins.nvim-cmp') end,
         }
 
--- files/nvim/lua/plugins/nvim-cmp.lua
+++ files/nvim/lua/plugins/nvim-cmp.lua
@@ -12,16 +12,25 @@ else
 end
 
 cmp.setup {
+    snippet = {
+        expand = function(args)
+            vim.fn['vsnip#anonymous'](args.body)
+        end,
+    },
     mapping = {
         -- Recommended key bindings from README
-        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        [autocomplete_key] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.close(),
+        ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+        ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+        [autocomplete_key] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+        ['<C-e>'] = cmp.mapping({
+            i = cmp.mapping.abort(),
+            c = cmp.mapping.close(),
+        }),
         ['<CR>'] = cmp.mapping.confirm({ select = true }),
     },
     sources = {
         { name = 'nvim_lsp' },
+        { name = 'vsnip' },
         { name = 'buffer' },
     },
     documentation = {
