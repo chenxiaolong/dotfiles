ed755fcbc85ae626613af0a3a99dc6c970ed23f7
Andrew Gunnerson <chillermillerlong@hotmail.com>
2022-01-19 18:05:55 -0500

    shell: Move OSC9;9 logic into 50_cwd.sh
    Signed-off-by: Andrew Gunnerson <chillermillerlong@hotmail.com>

--- files/shell/shell.d/50_cwd.sh
+++ files/shell/shell.d/50_cwd.sh
@@ -13,10 +13,20 @@ __osc7() {
     printf '\e]7;file://%s%s\e\\' "${HOSTNAME}" "$(__urlencode "${PWD}")"
 }
 
+__wsl_osc99() {
+    printf '\e]9;9;"%s"\e\\' "$(wslpath -w "${PWD}")"
+}
+
+if is_os WSL; then
+    __osc_func=__wsl_osc99
+else
+    __osc_func=__osc7
+fi
+
 if is_shell bash; then
     declare -a PROMPT_COMMAND
-    PROMPT_COMMAND+=(__osc7)
+    PROMPT_COMMAND+=("${__osc_func}")
 elif is_shell zsh; then
     autoload -Uz add-zsh-hook
-    add-zsh-hook -Uz chpwd __osc7
+    add-zsh-hook -Uz chpwd "${__osc_func}"
 fi
--- files/shell/shell.d/50_starship.sh
+++ files/shell/shell.d/50_starship.sh
@@ -12,14 +12,8 @@ if starship_path=$(command -v starship) && (is_shell bash || is_shell zsh); then
             literal_replace "${hostname}" "${hostname}(WSL:${WSL_DISTRO_NAME})"
         }
 
-        starship_wsl_osc99() {
-            # OSC 9;9
-            printf "\e]9;9;\"%s\"\e\\" "$(wslpath -w "$(pwd)")"
-        }
-
         if is_shell zsh; then
             starship_prompt_wrap() {
-                starship_wsl_osc99
                 "${starship_path}" "${@}" | starship_wsl_hostname_replace
             }
             PROMPT=$(literal_replace "${starship_path}" starship_prompt_wrap <<< "${PROMPT}")
@@ -28,7 +22,6 @@ if starship_path=$(command -v starship) && (is_shell bash || is_shell zsh); then
             starship_prompt_wrap() {
                 eval "${starship_prompt_command}"
                 PS1=$(starship_wsl_hostname_replace <<< "${PS1}")
-                starship_wsl_osc99
             }
             PROMPT_COMMAND=starship_prompt_wrap
         fi
