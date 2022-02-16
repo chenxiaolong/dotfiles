b2eca38b8555b7d92ae93c28f96726a4b00a6603
Andrew Gunnerson <chillermillerlong@hotmail.com>
2022-02-16 16:16:02 -0500

    nvim: feline: Update for theming API changes
    Signed-off-by: Andrew Gunnerson <chillermillerlong@hotmail.com>

--- files/nvim/lua/plugins/feline.lua
+++ files/nvim/lua/plugins/feline.lua
@@ -1,38 +1,4 @@
 local vi_mode_utils = require('feline.providers.vi_mode')
-local colorscheme = vim.api.nvim_exec('colorscheme', true)
-local theme = require('colorscheme').colorschemes[string.sub(colorscheme, 8)]
-
-local colors = {
-    bg = '#2d2d2d',
-    fg = theme.base05,
-    cyan = theme.base0C,
-    green = theme.base0B,
-    light_blue = theme.base0D,
-    light_gray = theme.base03,
-    orange = theme.base09,
-    purple = theme.base0E,
-    red = theme.base08,
-    white = theme.base07,
-    yellow = theme.base0A,
-}
-
-local vi_mode_colors = {
-    NORMAL = colors.green,
-    OP = colors.green,
-    INSERT = colors.yellow,
-    VISUAL = colors.purple,
-    LINES = colors.purple,
-    BLOCK = colors.purple,
-    REPLACE = colors.cyan,
-    ['V-REPLACE'] = colors.cyan,
-    ENTER = colors.cyan,
-    MORE = colors.cyan,
-    SELECT = colors.orange,
-    COMMAND = colors.red,
-    SHELL = colors.green,
-    TERM = colors.green,
-    NONE = colors.white,
-}
 
 local components = {
     active = {
@@ -59,7 +25,7 @@ local components = {
                 icon = '',
                 left_sep = ' ',
                 hl = {
-                    fg = colors.light_blue,
+                    fg = 'light_blue',
                 },
             },
             {
@@ -67,7 +33,7 @@ local components = {
                 icon = ' ',
                 left_sep = ' ',
                 hl = {
-                    fg = colors.purple,
+                    fg = 'purple',
                 },
             },
             {
@@ -75,7 +41,7 @@ local components = {
                 icon = '',
                 left_sep = ' ',
                 hl = {
-                    fg = colors.green,
+                    fg = 'green',
                 }
             },
             {
@@ -83,7 +49,7 @@ local components = {
                 icon = '',
                 left_sep = ' ',
                 hl = {
-                    fg = colors.orange,
+                    fg = 'orange',
                 }
             },
             {
@@ -91,7 +57,7 @@ local components = {
                 icon = '',
                 left_sep = ' ',
                 hl = {
-                    fg = colors.red,
+                    fg = 'red',
                 }
             },
         },
@@ -101,21 +67,21 @@ local components = {
                 icon = '',
                 right_sep = ' ',
                 hl = {
-                    fg = colors.yellow,
+                    fg = 'yellow',
                 },
             },
             {
                 provider = 'file_format',
                 right_sep = ' ',
                 hl = {
-                    fg = colors.light_blue,
+                    fg = 'light_blue',
                 },
             },
             {
                 provider = 'file_encoding',
                 right_sep = ' ',
                 hl = {
-                    fg = colors.green,
+                    fg = 'green',
                 },
             },
             {
@@ -127,21 +93,21 @@ local components = {
                 },
                 right_sep = ' ',
                 hl = {
-                    fg = colors.purple,
+                    fg = 'purple',
                 },
             },
             {
                 provider = 'position',
                 right_sep = ' ',
                 hl = {
-                    fg = colors.cyan,
+                    fg = 'cyan',
                 },
             },
             {
                 provider = 'line_percentage',
                 right_sep = ' ',
                 hl = {
-                    fg = colors.cyan,
+                    fg = 'cyan',
                 },
             },
         },
@@ -161,11 +127,39 @@ for section_index, section in ipairs(components.active) do
     end
 end
 
+local colorscheme = vim.api.nvim_exec('colorscheme', true)
+local base16_theme = require('colorscheme').colorschemes[string.sub(colorscheme, 8)]
+
 require('feline').setup {
-    colors = {
-        bg = colors.bg,
-        fg = colors.fg,
-    },
     components = components,
-    vi_mode_colors = vi_mode_colors,
+    theme = {
+        bg = base16_theme.base00,
+        fg = base16_theme.base05,
+        cyan = base16_theme.base0C,
+        green = base16_theme.base0B,
+        light_blue = base16_theme.base0D,
+        light_gray = base16_theme.base03,
+        orange = base16_theme.base09,
+        purple = base16_theme.base0E,
+        red = base16_theme.base08,
+        white = base16_theme.base07,
+        yellow = base16_theme.base0A,
+    },
+    vi_mode_colors = {
+        NORMAL = 'green',
+        OP = 'green',
+        INSERT = 'yellow',
+        VISUAL = 'purple',
+        LINES = 'purple',
+        BLOCK = 'purple',
+        REPLACE = 'cyan',
+        ['V-REPLACE'] = 'cyan',
+        ENTER = 'cyan',
+        MORE = 'cyan',
+        SELECT = 'orange',
+        COMMAND = 'red',
+        SHELL = 'green',
+        TERM = 'green',
+        NONE = 'white',
+    },
 }
