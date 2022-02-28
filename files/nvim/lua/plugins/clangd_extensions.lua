f0d4adacee52e635434fa1d58ee54d2213b8f34b
Andrew Gunnerson <chillermillerlong@hotmail.com>
2022-02-28 17:10:34 -0500

    nvim: offsetEncoding workaround is required for clangd
    Signed-off-by: Andrew Gunnerson <chillermillerlong@hotmail.com>

--- files/nvim/lua/plugins/clangd_extensions.lua
+++ files/nvim/lua/plugins/clangd_extensions.lua
@@ -1,1 +1,11 @@
-require('clangd_extensions').setup({})
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+-- https://github.com/neovim/neovim/pull/16694
+-- https://github.com/jose-elias-alvarez/null-ls.nvim/issues/428
+capabilities.offsetEncoding = { 'utf-16' }
+
+require('clangd_extensions').setup({
+    server = {
+        capabilities = capabilities,
+    },
+})
