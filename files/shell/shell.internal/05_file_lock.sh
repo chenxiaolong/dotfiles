51a5664a6748fea3cef8b46823f7a82ccb5188f8
Andrew Gunnerson <chillermillerlong@hotmail.com>
2022-03-14 16:42:36 -0400

    shell/shell.internal/05_file_lock.sh: Add perl implementation to support Git Bash on Windows
    Signed-off-by: Andrew Gunnerson <chillermillerlong@hotmail.com>

--- files/shell/shell.internal/05_file_lock.sh
+++ files/shell/shell.internal/05_file_lock.sh
@@ -3,6 +3,8 @@ flock_fd() {
 
     if command -v flock &>/dev/null; then
         flock "${fd}"
+    elif command -v perl &>/dev/null; then
+        perl -e 'use Fcntl qw(:flock); open(my $fh, "<&=", $ARGV[0]); flock($fh, LOCK_EX) || die "flock: $!";' "${fd}"
     else
         python -c 'import fcntl, sys; fcntl.flock(int(sys.argv[1]), fcntl.LOCK_EX)' "${fd}"
     fi
