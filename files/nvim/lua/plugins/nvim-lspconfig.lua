17cb724951ed91f6659cec691b4ee1260a596ed2
Andrew Gunnerson <chillermillerlong@hotmail.com>
2022-09-17 12:27:31 -0400

    nvim: lspconfig: Add pylsp
    Signed-off-by: Andrew Gunnerson <chillermillerlong@hotmail.com>

--- files/nvim/lua/plugins/nvim-lspconfig.lua
+++ files/nvim/lua/plugins/nvim-lspconfig.lua
@@ -2,3 +2,7 @@ local lspconfig = require('lspconfig')
 
 -- C++ is handled by clangd_extensions
 -- Rust is handled by rust-tools
+
+if vim.fn.executable('pylsp') == 1 then
+    lspconfig.pylsp.setup({})
+end
