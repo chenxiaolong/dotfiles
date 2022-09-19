4a81e0dd84e76d3f8d193707b5ce68fef8bcf9ea
Andrew Gunnerson <chillermillerlong@hotmail.com>
2022-09-19 12:25:20 -0400

    nvim: lspconfig: Add bashls
    Signed-off-by: Andrew Gunnerson <chillermillerlong@hotmail.com>

--- files/nvim/lua/plugins/nvim-lspconfig.lua
+++ files/nvim/lua/plugins/nvim-lspconfig.lua
@@ -3,6 +3,9 @@ local lspconfig = require('lspconfig')
 -- C++ is handled by clangd_extensions
 -- Rust is handled by rust-tools
 
+if vim.fn.executable('bash-language-server') == 1 then
+    lspconfig.bashls.setup({})
+end
 if vim.fn.executable('pylsp') == 1 then
     lspconfig.pylsp.setup({})
 end
