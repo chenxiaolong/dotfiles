9c59f6b3c39df75dc1f1572b4eeac35eb4eeb24f
Andrew Gunnerson <chillermillerlong@hotmail.com>
2022-11-14 20:03:49 -0500

    wezterm: Remove warning about missing glyphs in font
    Signed-off-by: Andrew Gunnerson <chillermillerlong@hotmail.com>

--- files/wezterm/wezterm.lua
+++ files/wezterm/wezterm.lua
@@ -28,6 +28,7 @@ local options = {
     colors = colors,
     font = wezterm.font('Fira Code'),
     font_size = 9,
+    warn_about_missing_glyphs = false,
     enable_kitty_keyboard = true,
     enable_scroll_bar = true,
     keys = {
