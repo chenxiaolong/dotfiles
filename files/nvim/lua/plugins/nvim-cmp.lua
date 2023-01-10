646227234e7f6533c7db42afca87b44ab0188200
Andrew Gunnerson <chillermillerlong@hotmail.com>
2023-01-10 16:18:05 -0500

    nvim: nvim-cmp: I do not use Windows anymore
    Signed-off-by: Andrew Gunnerson <chillermillerlong@hotmail.com>

--- files/nvim/lua/plugins/nvim-cmp.lua
+++ files/nvim/lua/plugins/nvim-cmp.lua
@@ -1,15 +1,4 @@
 local cmp = require('cmp')
-local autocomplete_key
-
--- https://github.com/neovim/neovim/issues/13680
--- https://github.com/libuv/libuv/pull/2535
--- https://github.com/microsoft/terminal/issues/2865
--- NOTE: Requires corresponding Windows Terminal setting
-if vim.fn.has('win32') then
-    autocomplete_key = '<C-S-Insert>'
-else
-    autocomplete_key = '<C-Space>'
-end
 
 cmp.setup {
     snippet = {
@@ -25,7 +14,7 @@ cmp.setup {
         -- Recommended key bindings from README
         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        [autocomplete_key] = cmp.mapping.complete(),
+        ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
         ['<CR>'] = cmp.mapping.confirm({ select = true }),
     }),
