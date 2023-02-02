9f9c2bdd433efaff13c08e8c553b388a28685acb
Andrew Gunnerson <accounts+github@chiller3.com>
2023-02-01 23:12:50 -0500

    shell: Use /etc/os-release for distro detection
    Signed-off-by: Andrew Gunnerson <accounts+github@chiller3.com>

--- files/shell/shell.internal/03_distro.sh
+++ files/shell/shell.internal/03_distro.sh
@@ -1,18 +1,6 @@
 if is_os Linux; then
     if [[ -z "${__distro}" ]]; then
-        if [ -f /etc/fedora-release ]; then
-            __distro=Fedora
-        elif [ -f /etc/arch-release ]; then
-            __distro=Arch
-        elif [ -f /etc/SuSE-release ]; then
-            __distro=openSUSE
-        elif [ -f /etc/debian_version ]; then
-            __distro=Debian
-        elif [ -f /etc/ubuntu_version ]; then
-            __distro=Ubuntu
-        else
-            __distro=Unknown
-        fi
+        __distro="$(source /etc/os-release && echo "${ID}")"
     fi
 
     is_distro() {
