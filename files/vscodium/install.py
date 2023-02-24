94ddbaab4e69ecdfb29b64d842682dd221e70eb7
Andrew Gunnerson <accounts+github@chiller3.com>
2023-02-24 17:50:31 -0500

    codium: install.py: Remove unlisted extensions
    Signed-off-by: Andrew Gunnerson <accounts+github@chiller3.com>

--- files/vscodium/install.py
+++ files/vscodium/install.py
@@ -11,16 +11,33 @@ def main():
     parser.add_argument('executable', help='VSCodium executable')
     args = parser.parse_args()
 
+    desired = set()
+
     with open(os.path.join(sys.path[0], 'extensions.txt')) as f:
         for line in f:
             line = line.strip()
             if not line or line.startswith('#'):
                 continue
 
-            subprocess.check_call([
-                args.executable,
-                '--install-extension', line,
-            ])
+            desired.add(line)
+
+    actual = set(subprocess.check_output([
+        args.executable,
+        '--list-extensions',
+    ]).decode('ascii').splitlines())
+
+    for ext in actual - desired:
+        subprocess.check_call([
+            args.executable,
+            '--uninstall-extension', ext,
+        ])
+    # DELETE
+
+    for ext in desired - actual:
+        subprocess.check_call([
+            args.executable,
+            '--install-extension', ext,
+        ])
 
 
 if __name__ == '__main__':
