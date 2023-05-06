94190b5063859ea1959075ce3a71d55b7c6807b6
Andrew Gunnerson <accounts+github@chiller3.com>
2023-05-06 18:47:48 -0400

    shell: Remove 40_env_java.sh
    Signed-off-by: Andrew Gunnerson <accounts+github@chiller3.com>

--- files/shell/shell.d/40_env_java.sh
+++
@@ -1,3 +0,0 @@
-if is_os Linux && ! is_distro ubuntu; then
-    export _JAVA_OPTIONS="-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel"
-fi
