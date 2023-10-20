c1400567d49b10ec64c16428422cbb1c4b9aaaba
Andrew Gunnerson <accounts+github@chiller3.com>
2023-10-20 15:47:13 -0400

    nvim: Remove gitsigns code actions for none-ls
    Signed-off-by: Andrew Gunnerson <accounts+github@chiller3.com>

--- files/nvim/lua/plugins/none-ls.lua
+++ files/nvim/lua/plugins/none-ls.lua
@@ -1,8 +1,6 @@
 null_ls = require('null-ls')
 
-local sources = {
-    null_ls.builtins.code_actions.gitsigns,
-}
+local sources = {}
 
 if vim.fn.executable('clang-format') == 1 then
     table.insert(sources, null_ls.builtins.formatting.clang_format)
