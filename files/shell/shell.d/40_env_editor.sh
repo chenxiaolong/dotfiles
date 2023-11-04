84b275970bb4841341451dabbd574ff8647f4a0e
Andrew Gunnerson <accounts+github@chiller3.com>
2023-11-04 17:14:21 -0400

    shell: Use helix as the default editor
    Signed-off-by: Andrew Gunnerson <accounts+github@chiller3.com>

--- files/shell/shell.d/40_env_editor.sh
+++ files/shell/shell.d/40_env_editor.sh
@@ -1,4 +1,6 @@
-if command -v nvim >/dev/null; then
+if command -v hx >/dev/null; then
+    export EDITOR=hx
+elif command -v nvim >/dev/null; then
     export EDITOR=nvim
 elif command -v vim >/dev/null; then
     export EDITOR=vim
