7b7ea258d7bd732042bdc115e2ceac8e82460980
Andrew Gunnerson <accounts+github@chiller3.com>
2024-07-27 22:22:35 -0400

    shell: 40_env_android.sh: Simplify to only handle modern SDK directory structure
    Signed-off-by: Andrew Gunnerson <accounts+github@chiller3.com>

--- files/shell/shell.d/40_env_android.sh
+++ files/shell/shell.d/40_env_android.sh
@@ -1,62 +1,33 @@
-set_up_android_sdk() {
-    local sdk_path
-
-    if [[ -n "${ANDROID_HOME}" ]]; then
-        sdk_path=${ANDROID_HOME}
-    elif [[ -d /opt/android-sdk ]]; then
-        sdk_path=/opt/android-sdk
+if [[ -n "${ANDROID_HOME}" ]]; then
+    if [[ ! -d "${ANDROID_HOME}" ]]; then
+        echo >&2 "WARNING: Android SDK path ${ANDROID_HOME} does not exist"
+        return 1
     fi
 
-    if [[ -n "${sdk_path}" ]]; then
-        if [[ ! -d "${sdk_path}" ]]; then
-            echo >&2 "WARNING: Android SDK path ${sdk_path} does not exist"
-            return 1
-        fi
+    path_push_front "${ANDROID_HOME}/tools"
+    path_push_front "${ANDROID_HOME}/tools/bin"
+    path_push_front "${ANDROID_HOME}/platform-tools"
 
-        export ANDROID_HOME=${sdk_path}
-
-        path_push_front "${sdk_path}/tools"
-        path_push_front "${sdk_path}/tools/bin"
-        path_push_front "${sdk_path}/platform-tools"
-
-        if [[ -d "${sdk_path}/build-tools" ]]; then
-            path_push_front "$(find "${sdk_path}/build-tools" \
-                               -mindepth 1 -maxdepth 1 -print0 \
-                               | sort -zV | tail -zn1 | tr -d '\0')"
-        fi
-
-        if [[ -d "${sdk_path}/cmdline-tools" ]]; then
-            path_push_front "${sdk_path}/cmdline-tools/latest/bin"
-        fi
-    fi
-}
-
-set_up_android_ndk() {
-    local ndk_path
-
-    if [[ -n "${ANDROID_NDK_ROOT}" ]]; then
-        ndk_path=${ANDROID_NDK_ROOT}
-    elif [[ -n "${ANDROID_HOME}" && -d "${ANDROID_HOME}/ndk" ]]; then
-        ndk_path=$(find "${ANDROID_HOME}/ndk" \
-                   -mindepth 1 -maxdepth 1 ! -name magisk -print0 \
-                   | sort -zV | tail -zn1 | tr -d '\0')
-    elif [[ -d /opt/android-ndk ]]; then
-        ndk_path=/opt/android-ndk
+    if [[ -d "${ANDROID_HOME}/build-tools" ]]; then
+        path_push_front "$(find "${ANDROID_HOME}/build-tools" \
+                           -mindepth 1 -maxdepth 1 -print0 \
+                           | sort -zV | tail -zn1 | tr -d '\0')"
     fi
 
-    if [[ -n "${ndk_path}" ]]; then
-        if [[ ! -d "${ndk_path}" ]]; then
-            echo >&2 "WARNING: Android NDK path ${ndk_path} does not exist"
-            return 1
-        fi
-
-        export ANDROID_NDK_ROOT=${ndk_path}
-        path_push_front "${ndk_path}"
+    if [[ -d "${ANDROID_HOME}/cmdline-tools" ]]; then
+        path_push_front "${ANDROID_HOME}/cmdline-tools/latest/bin"
     fi
-}
 
-set_up_android_sdk
-set_up_android_ndk
+    if [[ -z "${ANDROID_NDK_ROOT}" ]]; then
+        export ANDROID_NDK_ROOT=$(find "${ANDROID_HOME}/ndk" \
+                                  -mindepth 1 -maxdepth 1 ! \
+                                  -name magisk -print0 \
+                                  | sort -zV | tail -zn1 | tr -d '\0')
 
-unset -f set_up_android_sdk
-unset -f set_up_android_ndk
+        if [[ -d "${ANDROID_NDK_ROOT}" ]]; then
+            path_push_front "${ANDROID_NDK_ROOT}"
+        else
+            unset ANDROID_NDK_ROOT
+        fi
+    fi
+fi
