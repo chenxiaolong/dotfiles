8c5ff33d3ad84e556c65319e355b3b9f57c9beec
Andrew Gunnerson <accounts+github@chiller3.com>
2024-10-09 17:39:42 -0400

    nvim: nvim-lspconfig: Avoid hardcoding executable names
    Signed-off-by: Andrew Gunnerson <accounts+github@chiller3.com>

--- files/nvim/lua/plugins/nvim-lspconfig.lua
+++ files/nvim/lua/plugins/nvim-lspconfig.lua
@@ -2,23 +2,19 @@ local lspconfig = require('lspconfig')
 
 -- C++ is handled by clangd_extensions
 
-if vim.fn.executable('bash-language-server') == 1 then
-    lspconfig.bashls.setup({
-        autostart = false,
-    })
-end
-if vim.fn.executable('gopls') == 1 then
-    lspconfig.gopls.setup({
-        autostart = false,
-    })
-end
-if vim.fn.executable('pylsp') == 1 then
-    lspconfig.pylsp.setup({
-        autostart = false,
-    })
-end
-if vim.fn.executable('rust-analyzer') == 1 then
-    lspconfig.rust_analyzer.setup({
-        autostart = false,
-    })
+local lsps = {
+    'bashls',
+    'gopls',
+    'pylsp',
+    'rust_analyzer',
+}
+
+for _, lsp in ipairs(lsps) do
+    local cmd = lspconfig[lsp].config_def.default_config.cmd[1]
+
+    if vim.fn.executable(cmd) == 1 then
+        lspconfig[lsp].setup({
+            autostart = false,
+        })
+    end
 end
