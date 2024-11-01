f15ec4ce53ba381797b9405bd822c2ae275e3902
Andrew Gunnerson <accounts+github@chiller3.com>
2024-10-31 20:53:13 -0400

    nvim: guess-indent: Set shiftwidth=0 for files with tabs
    Make the tab key actually create tabs if the file uses tabs.
    
    Signed-off-by: Andrew Gunnerson <accounts+github@chiller3.com>

--- files/nvim/lua/plugins/guess-indent.lua
+++ files/nvim/lua/plugins/guess-indent.lua
@@ -1,1 +1,14 @@
-require('guess-indent').setup({})
+local guess_indent = require('guess-indent')
+
+local opts = {
+    on_tab_options = {
+        -- Make the tab key create tabstop-sized indentation.
+        shiftwidth = 0
+    }
+}
+
+require('guess-indent').setup(vim.tbl_deep_extend(
+    'force',
+    require('guess-indent.config').values,
+    opts
+))
