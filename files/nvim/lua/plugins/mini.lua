de36b8d3e31ace19580b1f23c6ac14e15c5b5097
Andrew Gunnerson <accounts+github@chiller3.com>
2025-03-01 19:13:19 -0500

    nvim: Enable mini.clue
    Signed-off-by: Andrew Gunnerson <accounts+github@chiller3.com>

--- files/nvim/lua/plugins/mini.lua
+++ files/nvim/lua/plugins/mini.lua
@@ -24,6 +24,56 @@ for name, options in pairs(color_utils.get_all_highlight_options()) do
     end
 end
 
+local clue = require('mini.clue')
+clue.setup({
+    -- Suggested configuration from docs.
+    clues = {
+        clue.gen_clues.builtin_completion(),
+        clue.gen_clues.g(),
+        clue.gen_clues.marks(),
+        clue.gen_clues.registers(),
+        clue.gen_clues.windows(),
+        clue.gen_clues.z(),
+    },
+    triggers = {
+        -- Leader triggers
+        { mode = 'n', keys = '<Leader>' },
+        { mode = 'x', keys = '<Leader>' },
+
+        -- Built-in completion
+        { mode = 'i', keys = '<C-x>' },
+
+        -- `g` key
+        { mode = 'n', keys = 'g' },
+        { mode = 'x', keys = 'g' },
+
+        -- Marks
+        { mode = 'n', keys = "'" },
+        { mode = 'n', keys = '`' },
+        { mode = 'x', keys = "'" },
+        { mode = 'x', keys = '`' },
+
+        -- Registers
+        { mode = 'n', keys = '"' },
+        { mode = 'x', keys = '"' },
+        { mode = 'i', keys = '<C-r>' },
+        { mode = 'c', keys = '<C-r>' },
+
+        -- Window commands
+        { mode = 'n', keys = '<C-w>' },
+
+        -- `z` key
+        { mode = 'n', keys = 'z' },
+        { mode = 'x', keys = 'z' },
+    },
+    window = {
+        delay = 0,
+        config = {
+            width = 'auto',
+        },
+    },
+})
+
 require('mini.comment').setup()
 require('mini.completion').setup()
 
