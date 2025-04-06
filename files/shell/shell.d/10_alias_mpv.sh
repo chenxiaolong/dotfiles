32a4fad224eb9ea6c2205576c9839b57f24abaab
Andrew Gunnerson <accounts+github@chiller3.com>
2025-04-06 18:16:15 -0400

    mpv: Enable HDR by default
    0.40.0 now supports HDR on Wayland via the color-management-v1 protocol.
    
    Signed-off-by: Andrew Gunnerson <accounts+github@chiller3.com>

--- files/shell/shell.d/10_alias_mpv.sh
+++ files/shell/shell.d/10_alias_mpv.sh
@@ -1,3 +1,3 @@
 if command -v mpv >/dev/null; then
-    alias_if_missing mpvhdr 'ENABLE_HDR_WSI=1 mpv --vo=gpu-next --target-colorspace-hint --gpu-api=vulkan --gpu-context=waylandvk'
+    alias_if_missing mpvhdr 'ENABLE_HDR_WSI=1 mpv --gpu-api=vulkan --gpu-context=waylandvk'
 fi
