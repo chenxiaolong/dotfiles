7ac66ea073b844f3211b7bc0a6d082bf7b6121c4
Andrew Gunnerson <accounts+github@chiller3.com>
2025-04-19 18:43:34 -0400

    shell: 40_env_man.sh: Reduce chance of hard-wrapping
    Otherwise, word breaks make it more difficult to search.
    
    Signed-off-by: Andrew Gunnerson <accounts+github@chiller3.com>

--- files/shell/shell.d/40_env_man.sh
+++ files/shell/shell.d/40_env_man.sh
@@ -1,5 +1,6 @@
 if command -v nvim >/dev/null; then
     export MANPAGER='nvim +Man!'
+    export MANWIDTH=999
 else
     # https://wiki.archlinux.org/title/Color_output_in_console#Using_less
     export MANPAGER='less -R --use-color -Dd+b -Dug'
