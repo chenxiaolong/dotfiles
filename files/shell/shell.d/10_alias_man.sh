8435577a76a86da5f75f52271c2b9cdb5839c36d
Andrew Gunnerson <accounts+github@chiller3.com>
2025-04-19 18:25:26 -0400

    shell: 10_alias_man.sh: Stop using bat and simplify `less` coloring
    bat colors things incorrectly too often and has bad handling of SIGINT.
    
    Signed-off-by: Andrew Gunnerson <accounts+github@chiller3.com>

--- files/shell/shell.d/10_alias_man.sh
+++ files/shell/shell.d/10_alias_man.sh
@@ -1,25 +1,6 @@
 man() {
-    (
-        if [[ -z "${MANPAGER}" ]]; then
-            if command -v bat >/dev/null; then
-                # base16 theme doesn't colorize much in man pages
-                export MANPAGER="sh -c 'col -bx | bat -l man -p'"
-                export MANROFFOPT='-c'
-            else
-                # From website:
-                # http://www.cyberciti.biz/faq/linux-unix-colored-man-pages-with-less-command/
-                # https://wiki.archlinux.org/index.php/Man_Page
-
-                export LESS_TERMCAP_mb="$(printf "\e[1;31m")"
-                export LESS_TERMCAP_md="$(printf "\e[1;31m")"
-                export LESS_TERMCAP_me="$(printf "\e[0m")"
-                export LESS_TERMCAP_se="$(printf "\e[0m")"
-                export LESS_TERMCAP_so="$(printf "\e[1;44;33m")"
-                export LESS_TERMCAP_ue="$(printf "\e[0m")"
-                export LESS_TERMCAP_us="$(printf "\e[1;32m")"
-            fi
-        fi
-
-        exec command man "${@}"
-    )
+    # https://wiki.archlinux.org/title/Color_output_in_console#Using_less
+    MANPAGER='less -R --use-color -Dd+b -Dug' \
+    MANROFFOPT='-P -c' \
+    command man "${@}"
 }
