1ea972ebfbe797bb63581b01354cdb740ff14378
Andrew Gunnerson <accounts+github@chiller3.com>
2025-04-19 18:33:56 -0400

    shell: 10_alias_man.sh: Use nvim as MANPAGER if installed
    Signed-off-by: Andrew Gunnerson <accounts+github@chiller3.com>

--- files/shell/shell.d/10_alias_man.sh
+++ files/shell/shell.d/10_alias_man.sh
@@ -1,6 +1,15 @@
 man() {
-    # https://wiki.archlinux.org/title/Color_output_in_console#Using_less
-    MANPAGER='less -R --use-color -Dd+b -Dug' \
-    MANROFFOPT='-P -c' \
+    local pager roffopt
+
+    if command -v nvim >/dev/null; then
+        pager='nvim +Man!'
+    else
+        # https://wiki.archlinux.org/title/Color_output_in_console#Using_less
+        pager='less -R --use-color -Dd+b -Dug'
+        roffopt='-P -c'
+    fi
+
+    MANPAGER="${pager}" \
+    MANROFFOPT="${roffopt}" \
     command man "${@}"
 }
