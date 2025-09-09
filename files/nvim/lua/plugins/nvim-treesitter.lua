d4d2d87e6a7ba15d9599cd56ed8f5f946ca031e7
Andrew Gunnerson <accounts+github@chiller3.com>
2025-09-09 00:13:04 -0400

    Update all submodules
    nvim-treesitter was rewritten and requires new configuration.
    
    Signed-off-by: Andrew Gunnerson <accounts+github@chiller3.com>

--- files/nvim/lua/plugins/nvim-treesitter.lua
+++ files/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,41 +1,22 @@
--- If I ever have to see "Press ENTER or type command to continue" due to a
--- failed treesitter parser installation again... A better way would be to use
--- vim.ui_attach() and watch for msg_show events, but that disables all of the
--- builtin message and cmdline handling.
-if not _G.print_orig then
-    _G.print_orig = _G.print
-end
-_G.print = function(...)
-    local caller = debug.getinfo(2)
-
-    if vim.endswith(caller.source, '/nvim-treesitter/install.lua') then
-        local stringified = {}
-
-        for _, arg in ipairs({ ... }) do
-            table.insert(stringified, tostring(arg))
-        end
-
-        vim.notify(table.concat(stringified, ' '), vim.log.levels.INFO)
-    else
-        _G.print_orig(...)
-    end
+-- Kernel 6.1 on the Google Pixel devices has a bug where it kernel panics from
+-- a corrupted canary value when the device runs out of memory. Avoid using too
+-- much memory by limiting the concurrency.
+local max_jobs = nil
+if not not os.getenv('ANDROID_ROOT') then
+    max_jobs = 2
 end
 
-require('nvim-treesitter.configs').setup({
-    ensure_installed = 'all',
-    -- Kernel 6.1 on the Pixel 9 Pro XL and Pixel Tablet has a bug where it
-    -- kernel panics from a corrupted canary value when the device runs out of
-    -- memory. Avoid using too much memory by making the installation
-    -- synchronous. There is no option to limit concurrency.
-    sync_install = not not os.getenv('ANDROID_ROOT'),
-    highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-    },
-    indent = {
-        enable = true,
-    },
+require('nvim-treesitter').install({ 'all' }, { max_jobs = max_jobs })
+
+vim.api.nvim_create_autocmd('FileType', {
+  callback = function()
+      -- The function fails if there is no treesitter parser available for the
+      -- file type.
+      pcall(vim.treesitter.start)
+  end,
 })
+
+vim.bo.indentexpr = 'v:lua.require"nvim-treesitter".indentexpr()'
 
 vim.wo.foldmethod = 'expr'
 vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
