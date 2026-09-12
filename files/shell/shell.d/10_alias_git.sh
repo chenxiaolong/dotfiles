ed68548566bf816f1d57e18254df5ce699e8ce31
Andrew Gunnerson <accounts+github@chiller3.com>
2026-09-12 13:45:54 -0400

    shell: 10_alias_git.sh: Avoid hardcoding HEAD
    Signed-off-by: Andrew Gunnerson <accounts+github@chiller3.com>

--- files/shell/shell.d/10_alias_git.sh
+++ files/shell/shell.d/10_alias_git.sh
@@ -1,14 +1,18 @@
 gdescribe() {
+    local ref=${1:-HEAD}
+
     if [[ $(git tag | wc -l) -gt 0 ]]; then
         git describe --long | sed -E "s/^v//g;s/([^-]*-g)/r\1/;s/-/./g"
     else
-        echo "r$(git rev-list --count HEAD).$(git rev-parse --short HEAD)"
+        echo "r$(git rev-list --count "${ref}").$(git rev-parse --short "${ref}")"
     fi
 }
 
 garchive() {
+    local ref=${1:-HEAD}
+
     local name
-    name="$(basename "$(pwd)")-$(gdescribe)"
+    name="$(basename "$(pwd)")-$(gdescribe "${ref}")"
 
     git archive \
         -o "${name}".tar.gz \
@@ -16,5 +20,5 @@ garchive() {
         --format=tar.gz \
         -9 \
         -v \
-        HEAD
+        "${ref}"
 }
